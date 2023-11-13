ac959b0a3c7157348b8ef71ece910713df50b578
sierrawebid <sierrawebid@gmail.com>
2023-11-13 16:09:14 +0800

    Update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,12 +3,16 @@
 		<!-- svelte-ignore a11y-missing-attribute -->
 		<a class="navbar-brand">Navbar</a>
 
-		<button class="btn btn-outline-success" type="submit">Input</button>
+		<button
+			class="btn btn-sm btn-outline-warning"
+			data-bs-toggle="modal"
+			data-bs-target="#exampleModal">Input</button
+		>
 	</div>
 </nav>
 
 <style>
-    a{
-        color: #ffffff;
-    }
+	a {
+		color: #ffffff;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
-	const security = [
-		'Imran Mokodompit',
-		'Faisal Manopode',
-		'Karter Tanduk',
-		'Jerry Legoh',
-		'Refiansa Mokodompit',
-		'Jonly Tulandi'
+	const dataPinjaman = [
+		{ nama: 'Imran Mokodompit', pinjaman: 3000000, dibayar: 0 },
+		{ nama: 'Faisal Manopode', pinjaman: 2500000, dibayar: 0 },
+		{ nama: 'Karter Tanduk', pinjaman: 4000000, dibayar: 0 },
+		{ nama: 'Jerry Legoh', pinjaman: 3500000, dibayar: 0 },
+		{ nama: 'Refiansa Mokodompit', pinjaman: 500000, dibayar: 0 },
+		{ nama: 'Jonly Tulandi', pinjaman: 2000000, dibayar: 500000 },
+		{ nama: 'Michael Baranoy', pinjaman: 3000000, dibayar: 2700000 }
 	];
 </script>
 
@@ -22,13 +23,13 @@
 				</tr>
 			</thead>
 			<tbody>
-				{#each security as sec, index}
+				{#each dataPinjaman as sec, index}
 					<tr>
 						<td class="text-center align-middle">{index + 1}</td>
-						<td>{sec}</td>
-						<td class="text-center align-middle">Otto</td>
-						<td class="text-center align-middle">@mdo</td>
-						<td class="text-center align-middle">@mdo</td>
+						<td>{sec.nama}</td>
+						<td class="text-center align-middle">{sec.pinjaman}</td>
+						<td class="text-center align-middle">{sec.dibayar}</td>
+						<td class="text-center align-middle">{sec.pinjaman - sec.dibayar}</td>
 					</tr>
 				{/each}
 			</tbody>
