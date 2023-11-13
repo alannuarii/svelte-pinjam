3f0d513e31b46db7e715958cadfcb954be0b270a
sierrawebid <sierrawebid@gmail.com>
2023-11-13 11:38:22 +0800

    first commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<nav class="navbar bg-dark">
+	<div class="container-fluid">
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<a class="navbar-brand">Navbar</a>
+
+		<button class="btn btn-outline-success" type="submit">Search</button>
+	</div>
+</nav>
+
+<style>
+    a{
+        color: #ffffff;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	const security = [
+		'Imran Mokodompit',
+		'Faisal Manopode',
+		'Karter Tanduk',
+		'Jerry Legoh',
+		'Refiansa Mokodompit',
+		'Jonly Tulandi'
+	];
+</script>
+
+<section>
+	<div class="table-responsive">
+		<table class="table">
+			<thead>
+				<tr class="text-center">
+					<th scope="col">No</th>
+					<th scope="col">Nama</th>
+					<th scope="col">Pinjaman</th>
+					<th scope="col">Dibayar</th>
+					<th scope="col">Sisa</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each security as sec, index}
+					<tr>
+						<td class="text-center align-middle">{index + 1}</td>
+						<td>{sec}</td>
+						<td class="text-center align-middle">Otto</td>
+						<td class="text-center align-middle">@mdo</td>
+						<td class="text-center align-middle">@mdo</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</section>
