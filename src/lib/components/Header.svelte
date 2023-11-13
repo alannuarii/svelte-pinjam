7be41bbb5c7a9d1d53149f01144a21e67f2fc9bc
sierrawebid <sierrawebid@gmail.com>
2023-11-13 16:48:48 +0800

    Update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,7 @@
+<script>
+	import ModalInput from './ModalInput.svelte';
+</script>
+
 <nav class="navbar bg-dark">
 	<div class="container-fluid">
 		<!-- svelte-ignore a11y-missing-attribute -->
@@ -8,6 +12,7 @@
 			data-bs-toggle="modal"
 			data-bs-target="#exampleModal">Input</button
 		>
+		<ModalInput />
 	</div>
 </nav>
 
--- src/lib/components/ModalInput.svelte
+++ src/lib/components/ModalInput.svelte
@@ -5,13 +5,32 @@
 	aria-labelledby="exampleModalLabel"
 	aria-hidden="true"
 >
-	<div class="modal-dialog">
+	<div class="modal-dialog modal-dialog-centered">
 		<div class="modal-content">
 			<div class="modal-header">
 				<h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
 			</div>
-			<div class="modal-body">...</div>
+			<div class="modal-body mx-3">
+				<div class="mb-3">
+					<label for="exampleFormControlInput1" class="form-label">Nama</label>
+					<select class="form-select" aria-label="Default select example">
+						<option selected>Open this select menu</option>
+						<option value="1">One</option>
+						<option value="2">Two</option>
+						<option value="3">Three</option>
+					</select>
+				</div>
+				<div class="mb-3">
+					<label for="exampleFormControlInput1" class="form-label">Nilai</label>
+					<input
+						type="email"
+						class="form-control"
+						id="exampleFormControlInput1"
+						placeholder="name@example.com"
+					/>
+				</div>
+			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 				<button type="button" class="btn btn-primary">Save changes</button>
